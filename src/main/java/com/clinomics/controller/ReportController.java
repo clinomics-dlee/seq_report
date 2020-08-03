d0da0516b032695e18478a2f0e34828e467c5999
eastpeople-clinomics <eastpeople@clinomics.co.kr>
2020-08-03 18:01:05 +0900

    HTML 레포트 생성

--- src/main/java/com/clinomics/controller/ReportController.java
+++ src/main/java/com/clinomics/controller/ReportController.java
@@ -1,24 +1,34 @@
 package com.clinomics.controller;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.clinomics.service.ReportService;
 import com.clinomics.service.setting.MemberService;
-import com.google.common.collect.Maps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Controller
 public class ReportController {
@@ -26,92 +36,75 @@ public class ReportController {
 	@Autowired
 	MemberService userService;
 
+	@Autowired
+	ReportService reportService;
+	
+	@Value("${seq.workspacePath}")
+    private String workspacePath;
+
 	@GetMapping("/report")
 	public String getReport(@RequestParam Map<String, String> params, Model model) {
 		String filePath = params.get("filePath");
 		System.out.println("★★★★★★★★★★ filePath=" + filePath);
 
-		String workspace = "/workspace" + filePath.substring(filePath.lastIndexOf("/"));
-		String customer = "";
-		String service = "";
-
-		String outString = this.getOutString(new File(filePath + "/statistics.out"));
-
-		String[] tableDatas = outString.split("\n\n");
-
-		Map<String, Object> datas = Maps.newHashMap();
-		for (int i = 0; i < tableDatas.length; i++) {
-			String tableData = tableDatas[i];
-			String[] rows = tableData.split("\n");
-
-			String headerRow = rows[1];
-			int headerCount = headerRow.split("\t").length;
-			List<List<String>> tableRows = new ArrayList<List<String>>();
-			for (int j = 2; j < rows.length; j++) {
-				String row = rows[j];
-				String[] valueArray = row.split("\t");
-
-				List<String> values = new ArrayList<String>(Arrays.asList(valueArray));
-
-				// #. header 보다 값의 수가 적은 경우 모자란 수 만큼 공백값을 추가
-				if (headerCount > values.size()) {
-					int diff = headerCount - values.size();
-					for (int k = 0 ; k < diff; k++) {
-						values.add("");
-					}
-				}
-
-				// #. 첫번째 테이블에 첫번째 값과 마지막 값은 고정 셋팅
-				if (i == 0 && j == 2) {
-					customer = values.get(0);
-					service = values.get(5);
-				}
-
-				tableRows.add(values);
-			}
-			datas.put("tableData" + i, tableRows);
-		}
-
-		model.addAttribute("datas", datas);
-		model.addAttribute("customer", customer);
-		model.addAttribute("service", service);
-		model.addAttribute("filePath", filePath);
-		model.addAttribute("workspace", workspace);
+		Map<String, Object> info = reportService.getReportInfo(filePath);
+		
+		model.addAttribute("datas", info.get("datas"));
+		model.addAttribute("customer", info.get("customer"));
+		model.addAttribute("service", info.get("service"));
+		model.addAttribute("filePath", info.get("filePath"));
+		model.addAttribute("workspace", info.get("workspace"));
 
 		return "report/print";
+	}
+
+	@GetMapping("/report/test")
+	public ResponseEntity<Resource> getTestPage(@RequestParam Map<String, String> params
+		, HttpServletRequest request, HttpServletResponse response) {
+
+		String filePath = params.get("filePath");
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setCacheable(false);
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+
+		templateEngine.setTemplateResolver(templateResolver);
+		
+		final WebContext ctx = new WebContext(request, response, request.getServletContext());
+		Map<String, Object> info = reportService.getReportInfo(filePath);
+
+		ctx.setVariable("datas", info.get("datas"));
+		ctx.setVariable("customer", info.get("customer"));
+		ctx.setVariable("service", info.get("service"));
+		ctx.setVariable("filePath", info.get("filePath"));
+		ctx.setVariable("workspace", filePath);
+
+		String result = templateEngine.process("report/print", ctx);
+
+		result = reportService.imageToBase64String(result);
+		
+		InputStream targetStream = new ByteArrayInputStream(result.getBytes());
+		InputStreamResource resource = new InputStreamResource(targetStream);
+
+		HttpHeaders header = new HttpHeaders();
+        header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=test.html");
+        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        header.add("Pragma", "no-cache");
+		header.add("Expires", "0");
+		
+		return ResponseEntity.ok()
+				.headers(header)
+				.contentLength(result.getBytes().length)
+				.contentType(MediaType.APPLICATION_OCTET_STREAM)
+				.body(resource);
+		
 	}
 
 	@GetMapping("/report/{page}")
 	public String getReportPage(@PathVariable String page, Model model) {
 
 		return "report/" + page;
-	}
-
-	// ############################ private
-	private String getOutString(File outFile) {
-		String outString = "";
-        BufferedReader br = null;
-		try {
-            // #. read csv 데이터 파일
-            br = new BufferedReader(new FileReader(outFile));
-            String sLine = null;
-            while((sLine = br.readLine()) != null) {
-                outString += sLine + "\n";
-            }
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		return outString;
 	}
 }
---
+++ src/main/java/com/clinomics/service/ReportService.java
@@ -0,0 +1,165 @@
+package com.clinomics.service;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.common.collect.Maps;
+
+import org.apache.commons.net.util.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportService {
+    
+    private static final Logger logger = LoggerFactory.getLogger(ReportService.class);
+    
+    public Map<String, Object> getReportInfo(String filePath) {
+        Map<String, Object> rtn = Maps.newHashMap();
+        String workspace = "/workspace" + filePath.substring(filePath.lastIndexOf("/"));
+		String customer = "";
+		String service = "";
+
+		String outString = this.getOutString(new File(filePath + "/statistics.out"));
+
+		String[] tableDatas = outString.split("\n\n");
+
+		Map<String, Object> datas = Maps.newHashMap();
+		for (int i = 0; i < tableDatas.length; i++) {
+			String tableData = tableDatas[i];
+			String[] rows = tableData.split("\n");
+
+			String headerRow = rows[1];
+			int headerCount = headerRow.split("\t").length;
+			List<List<String>> tableRows = new ArrayList<List<String>>();
+			for (int j = 2; j < rows.length; j++) {
+				String row = rows[j];
+				String[] valueArray = row.split("\t");
+
+				List<String> values = new ArrayList<String>(Arrays.asList(valueArray));
+
+				// #. header 보다 값의 수가 적은 경우 모자란 수 만큼 공백값을 추가
+				if (headerCount > values.size()) {
+					int diff = headerCount - values.size();
+					for (int k = 0 ; k < diff; k++) {
+						values.add("");
+					}
+				}
+
+				// #. 첫번째 테이블에 첫번째 값과 마지막 값은 고정 셋팅
+				if (i == 0 && j == 2) {
+					customer = values.get(0);
+					service = values.get(5);
+				}
+
+				tableRows.add(values);
+			}
+			datas.put("tableData" + i, tableRows);
+        }
+        rtn.put("datas", datas);
+        rtn.put("customer", customer);
+        rtn.put("service", service);
+        rtn.put("filePath", filePath);
+        rtn.put("workspace", workspace);
+
+        return rtn;
+    }
+
+    private String getOutString(File outFile) {
+		String outString = "";
+        BufferedReader br = null;
+		try {
+            // #. read csv 데이터 파일
+            br = new BufferedReader(new FileReader(outFile));
+            String sLine = null;
+            while((sLine = br.readLine()) != null) {
+                outString += sLine + "\n";
+            }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return outString;
+    }
+    
+    public String imageToBase64String(String content) {
+
+		Pattern p = Pattern.compile("src=\"(.*?)\"");
+		Matcher m = p.matcher(content);
+		StringBuffer sb = new StringBuffer();
+
+		while (m.find()) {
+
+			String filePathName = m.group(1);
+			if (filePathName.startsWith("data")) continue;
+			String fileExtName = filePathName.substring(filePathName.lastIndexOf(".") + 1);
+
+			FileInputStream inputStream = null;
+			ByteArrayOutputStream byteOutStream = null;
+
+            String replacement = "";
+			try {
+				File file = new File(filePathName);
+				logger.info(filePathName);
+
+				if (file.exists()) {
+					logger.info("filePathName exists");
+					inputStream = new FileInputStream(file);
+					byteOutStream = new ByteArrayOutputStream();
+
+					int len = 0;
+					byte[] buf = new byte[1024];
+					while ((len = inputStream.read(buf)) != -1) {
+						byteOutStream.write(buf, 0, len);
+					}
+
+					byte[] fileArray = byteOutStream.toByteArray();
+					String imageString = new String(Base64.encodeBase64(fileArray));
+
+					replacement = "src=\"data:image/" + fileExtName + ";base64, " + imageString + "\"";
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+                    if (inputStream != null) {
+                        inputStream.close();
+                    }
+                    if (byteOutStream != null) {
+                        byteOutStream.close();
+                    }
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            
+            m.appendReplacement(sb, replacement);
+		}
+		m.appendTail(sb);
+
+		return sb.toString();
+	}
+}
